fdfa4704b732c8ff41be34bcde6cbe1dce2a2fe2
Martin Joshua Garaza <joshua070915@gmail.com>
2024-01-04 11:55:32 +0800

    01-04-2023

--- app/Http/Livewire/Reports/GeneralDashboard.php
+++ app/Http/Livewire/Reports/GeneralDashboard.php
@@ -2,39 +2,52 @@
 
 namespace App\Http\Livewire\Reports;
 
-use Livewire\Component;
 use App\Models\CookingShow;
 use App\Models\Setting;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
 
 class GeneralDashboard extends Component
 {
+    public $from, $to;
+
     public function render()
     {
-        $booked_shows = CookingShow::where('result', 'Booked')
-                            ->where('user_id', Auth::user()->user_id)
-                            ->count();
+        $from = Carbon::parse($this->from)->startOfMonth()->format('Y-m-d');
+        $to = Carbon::parse($this->to)->endOfMonth()->format('Y-m-d');
 
-        $expired_shows = CookingShow::where('result', 'Expired')
-                            ->where('user_id', Auth::user()->user_id)
-                            ->count();
+        $booked_shows = CookingShow::where('result', 'Booked')->whereBetween('date', [$from, $to]);
+        $expired_shows = CookingShow::where('result', 'Expired')->whereBetween('date', [$from, $to]);
+        $cooked_shows = CookingShow::whereRaw('(result = "Closed" OR result = "For Follow Up")')->whereBetween('date', [$from, $to]);
+        $closed_shows = CookingShow::where('result', 'Closed')->whereBetween('date', [$from, $to]);
 
-        $cooked_shows = CookingShow::whereRaw('(result = "Closed" OR result = "For Follow Up")')
-                            ->where('user_id', Auth::user()->user_id)
-                            ->count();
+        if (!User::find(Auth::user()->user_id)->hasRole('admin')) {
+            $booked_shows->where('user_id', Auth::user()->user_id);
 
-        $closed_shows = CookingShow::where('result', 'Closed')
-                            ->where('user_id', Auth::user()->user_id);
+            $expired_shows->where('user_id', Auth::user()->user_id);
+
+            $cooked_shows->where('user_id', Auth::user()->user_id);
+
+            $closed_shows->where('user_id', Auth::user()->user_id);
+        }
 
         $settings = Setting::first();
 
         return view('livewire.reports.general-dashboard', [
             'settings' => $settings,
-            'booked_shows' => $booked_shows,
-            'expired_shows' => $expired_shows,
-            'cooked_shows' => $cooked_shows,
+            'booked_shows' => $booked_shows->count(),
+            'expired_shows' => $expired_shows->count(),
+            'cooked_shows' => $cooked_shows->count(),
             'closed_shows' => $closed_shows->count(),
             'sets_sold' => $closed_shows->sum('amount_sold'),
         ]);
+    }
+
+    public function mount()
+    {
+        $this->from = Carbon::parse(now())->startOfMonth()->format('Y-m-d');
+        $this->to = Carbon::parse(now())->endOfMonth()->format('Y-m-d');
     }
 }
--- resources/views/livewire/reports/general-dashboard.blade.php
+++ resources/views/livewire/reports/general-dashboard.blade.php
@@ -11,42 +11,59 @@
 
 <div class="py-12 ">
     <div class="mx-auto sm:px-6 lg:px-8">
+        <div class="flex justify-end space-x-2">
+            <div class="form-control">
+                <label class="input-group input-group-sm">
+                    <span>From</span>
+                    <input type="date" class="input input-bordered input-sm" wire:model.lazy="from" />
+                </label>
+            </div>
+            <div class="form-control">
+                <label class="input-group input-group-sm">
+                    <span>To</span>
+                    <input type="date" class="input input-bordered input-sm" wire:model.lazy="to" />
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="mx-auto sm:px-6 lg:px-8">
         <div class="p-5 overflow-hidden sm:rounded-lg ">
             <div class="grid grid-cols-1 gap-6 md:grid-cols-4">
                 <div class="text-white shadow-lg bg-secondary card">
                     <div class="card-body">
                         <h2 class="card-title">Booked Shows</h2>
-                        <p class="text-5xl text-right">{{$booked_shows}}</p>
+                        <p class="text-5xl text-right">{{ $booked_shows }}</p>
                     </div>
                 </div>
                 <div class="text-white shadow-lg bg-error card">
                     <div class="card-body">
                         <h2 class="card-title">Expired Shows</h2>
-                        <p class="text-5xl text-right">{{$expired_shows}}</p>
+                        <p class="text-5xl text-right">{{ $expired_shows }}</p>
                     </div>
                 </div>
                 <div class="text-white shadow-lg bg-primary card">
                     <div class="card-body">
                         <h2 class="card-title">Cooked Shows</h2>
-                        <p class="text-5xl text-right">{{$cooked_shows}}</p>
+                        <p class="text-5xl text-right">{{ $cooked_shows }}</p>
                     </div>
                 </div>
                 <div class="text-white shadow-lg bg-success card">
                     <div class="card-body">
                         <h2 class="card-title">Closed Shows</h2>
-                        <p class="text-5xl text-right">{{$closed_shows}}</p>
+                        <p class="text-5xl text-right">{{ $closed_shows }}</p>
                     </div>
                 </div>
-                <div class="text-white shadow-lg bg-warning card">
+                <div class="text-white shadow-lg bg-accent card">
                     <div class="card-body">
                         <h2 class="card-title">Amount Sold (Reported)</h2>
-                        <p class="text-5xl text-right">{{number_format($sets_sold, 2)}}</p>
+                        <p class="text-5xl text-right">{{ number_format($sets_sold, 2) }}</p>
                     </div>
                 </div>
                 <div class="text-white shadow-lg bg-info card">
                     <div class="card-body">
                         <h2 class="card-title">Set Sold (Reported)</h2>
-                        <p class="text-5xl text-right">{{$sets_sold ? $sets_sold/$settings->set_amount : 0}}</p>
+                        <p class="text-5xl text-right">
+                            {{ $sets_sold ? number_format($sets_sold / $settings->set_amount, 2) : 0 }}</p>
                     </div>
                 </div>
             </div>
